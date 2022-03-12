37d23c9e0615f7f68f10c087a9115dd83b192576
migueldeoleiros <migueldeoleiros@gmail.com>
2022-03-12 13:25:47 +0100

    Deleted equals

--- src/es/udc/sistemasinteligentes/g1_42/Nodo.java
+++ src/es/udc/sistemasinteligentes/g1_42/Nodo.java
@@ -16,19 +16,10 @@ public class Nodo{
                 ", " + accion +
                 ')';
     }
+
     public Nodo(Nodo padre, Estado estado, Accion accion) {
         this.padre = padre;
         this.estado = estado;
         this.accion = accion;
-    }    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Nodo)) return false;
-        Nodo nodo = (Nodo) o;
-        return Objects.equals(estado, nodo.estado);
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(padre, estado, accion);
     }
 }
